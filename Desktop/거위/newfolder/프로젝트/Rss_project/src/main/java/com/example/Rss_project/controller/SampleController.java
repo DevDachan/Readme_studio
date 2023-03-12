a6d48fc9e8f468904be0b84ce65ce83db95308f8
Minsoo Kim <holiday1883@gmail.com>
2023-03-12 21:57:41 +0900

    Feat: Change contributor

--- Desktop/거위/newfolder/프로젝트/Rss_project/src/main/java/com/example/Rss_project/controller/SampleController.java
+++ Desktop/거위/newfolder/프로젝트/Rss_project/src/main/java/com/example/Rss_project/controller/SampleController.java
@@ -27,12 +27,51 @@ public class SampleController {
 
     @GetMapping(value = "/template/{templateNum}")
     public String getTemplate(@PathVariable String templateNum) {
-        long startTime = System.currentTimeMillis();
         TemplateDTO templateDTO = templateService.getTemplate(templateNum);
         String temp=templateDTO.getTemplateContributor();
-        temp= temp.replace("repositoryName","NodeJs-TeamCCService");
-        temp= temp.replace("userName","Devdachan");
-        return temp;
+        temp= temp.replace("repositoryName","Eta-scheduling");
+        temp= temp.replace("userName","geodo2");
+        String sample_Data=
+                "![header](https://capsule-render.vercel.app/api?type=Waving&color=auto&height=300&section=header&text=Readme%20Studio&fontSize=90)\n" +
+                "<div align=center><h1>\uD83D\uDCDA  STACKS</h1></div>\n" +
+                "<div align=center> \n" +
+                "  <img src=\"https://img.shields.io/badge/Spring-007396?style=for-the-badge&logo=Spring&logoColor=white\">\n" +
+                "  <br>\n" +
+                "  \n" +
+                "\u200B\n" +
+                "</div>\n" +
+                "\u200B\n" +
+                "## :one: 소개\n" +
+                "  이 프로젝트는 Readme Studio 서비스로  Spring Boot로 제작된 프로젝트입니다.\n" +
+                "## :two: 패키지 프레임워크 설치\n" +
+                "\u200B\n" +
+                "<b>clone 프로젝트 </b>\n" +
+                "```xml\n" +
+                "gh repo clone https://soominkiminsoo/SurveyForm1.git\n" +
+                "```\n" +
+                " <b>jar 파일 다운로드</b>\n" +
+                "* mysql-connector-j-8.0.31.jar\n" +
+                "* mail-1.4.7.jar\n" +
+                "* activation.jar\n" +
+                "   \n" +
+                "\u200B\n" +
+                "## :three:DB 구조\n" +
+                "C:.<br>\n" +
+                "├─dao<br>\n" +
+                "│    └─Impl<br>\n" +
+                "├─dto<br>\n" +
+                "├─entity<br>\n" +
+                "├─handler<br>\n" +
+                "│    └─Impl<br>\n" +
+                "├─repository<br>\n" +
+                "└─service<br>\n" +
+                "       └─Impl<br>\n" +
+                "\u200B\n" +
+                "## :four:쿼리 메소드\n" +
+                "\u200B\n" +
+                "## :five:Contributor\n" +
+                "\u200B\n" + temp;
+        return sample_Data;
     }
 
     @RequestMapping(value = "/readme")
