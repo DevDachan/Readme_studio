1aa569ada432d8461da14e9e392e5c43a3e8aa7f
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-03-17 04:07:25 +0900

    refact: change info because of merge error

--- backend/src/main/java/com/readme/rss/controller/UnzipController.java
+++ backend/src/main/java/com/readme/rss/controller/UnzipController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3005")
 @RestController
 // @RequestMapping("/readme")
 public class UnzipController {
