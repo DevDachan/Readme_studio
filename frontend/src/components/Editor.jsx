3a53550996f2d4e1b7c6452451e8120daa9d96cf
DevDachan <dachan9524@gmail.com>
2023-03-17 13:26:21 +0900

    feat: edit editor.jsx

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, useLocation  } from "react-router-dom";
 import styled from "styled-components";
+import axios from "axios";
 
 import ReadmeFileList from "./File/ReadmeFileList";
 import ReadmeFileContent from "./File/ReadmeFileContent";
@@ -21,9 +22,42 @@ function Editor(props) {
   const navigate = useNavigate();
   const location = useLocation();
 
+  const [readmeObject, setReadmeObject] = useState();
+  const [currentReadme, setCurrentReadme] = useState();
+
+  const index = location.index;
+  const constrollerList = location.templateList;
+
   const goMain = (e) =>{
     navigate('../');
   }
+
+
+  const getTemplate = (e) =>{
+
+    const formData = new FormData();
+    formData.append('index', index);
+    formData.append('template_id', e.number);
+
+    axios({
+      method: "get",
+      url: 'http://localhost:8090/plzTemplate',
+      data: formData
+    })
+    .then(function (response){
+      //handle success
+      setReadmeObject(response);
+    })
+    .catch(function(error){
+      //handle error
+      console.log(error);
+    })
+    .then(function(){
+      // always executed
+    });
+  }
+
+
 
   return (
       <Wrapper>
@@ -38,13 +72,16 @@ function Editor(props) {
           <div className="col-sm-9">
           </div>
 
-          <div className="col-sm-8">
+          <div className="col-sm-8 mb-4">
             <ReadmeFileContent />
           </div>
-          <div className="col-sm-4">
+          <div className="col-sm-4 mb-4">
             <Controller />
           </div>
-          <div className="col-sm-12">
+          <div className="col-sm-12 calign mb-3">
+            <input type="button" className="bt-back" value="Generate" onClick={goMain} />
+          </div>
+          <div className="col-sm-12 calign mb-2">
             <input type="button" className="bt-back" value="Back" onClick={goMain} />
           </div>
         </div>
