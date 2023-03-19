67a384d1fbe43a03426d44275431df2844d76a7a
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-03-19 13:35:28 +0900

    feat: add data package code about Framework table

---
+++ backend/src/main/java/com/readme/rss/data/repository/FrameworkRepository.java
@@ -0,0 +1,8 @@
+package com.readme.rss.data.repository;
+
+import com.readme.rss.data.entity.FrameworkEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FrameworkRepository extends JpaRepository<FrameworkEntity, String> {
+
+}
