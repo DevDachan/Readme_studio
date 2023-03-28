1a4e01c5ccf73452f415060f9e0852ced3a3df66
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-03-28 22:32:41 +0900

    feat: merge backend code(all data & period)

--- backend/src/main/java/com/readme/rss/controller/UnzipController.java
+++ backend/src/main/java/com/readme/rss/controller/UnzipController.java
@@ -286,4 +286,83 @@ public class UnzipController {
 
         return frame_content;
     }
+
+    @PostMapping("/alldata")
+    public String alldata(@RequestParam("project_id") String project_id,
+        @RequestParam("framework_name") String framework_name) {
+        // 여기서 사용자가 누구인지 index값으로 알아내기
+        String frame_content = "";
+        System.out.println(project_id+framework_name+"파라미터 체크");
+        UserDTO userDTO = userService.getUser(project_id);
+        String user_name = userDTO.getUser_name();
+        String repo_name = userDTO.getRepository_name();
+
+        frame_content = "Header_check\n" +
+            "\n" +
+            "\n" +
+            "Contributor_check\n" +
+            "<div style=\"font-weight:bold; font-size: 21px;\">Project Period</div>" +
+            "<div><img src=\"PeriodImage\" width=100%></div>" +
+            "<span style=\"width:20%\"><span/>" +
+            "<span style=\"margin-right: 55%; margin-left: 5%;\">startDate</span>" +
+            "<span width=20%>endDate</span>" +
+            "\n" +
+            "Period_check\n" +
+            "\n" +
+            "\n" +
+            "---\n" +
+            "## Table of contents[![](https://raw.githubusercontent.com/aregtech/areg-sdk/master/docs/img/pin.svg)](#table-of-contents)\n" +
+            "- [Install](#install)\n" +
+            "- [DB](#db)\n" +
+            "- [queryMethod](#querymethod)\n" +
+            "---\n" +
+            "\n" +
+            "## Install[![](https://raw.githubusercontent.com/aregtech/areg-sdk/master/docs/img/pin.svg)](#install)\n" +
+            "<div align=\"right\">[ <a href=\"#table-of-contents\">↑ to top ↑</a> ]</div>\n" +
+            "1. Java 설치\n" +
+            "   - Spring Boot를 사용하려면 Java 8 버전 이상이 필요합니다.<br><br />\n" +
+            "   - [Oracle Java](https://www.oracle.com/technetwork/java/javase/downloads/index.html) .<br><br />\n" +
+            "\n" +
+            "\n" +
+            "```\n" +
+            "sdk install spring boot\n" +
+            "```\n" +
+            "2. Spring Boot CLI 설치\n" +
+            "\n" +
+            "   Spring Boot CLI는 Spring Boot 애플리케이션을 빠르게 만들 수 있는 명령줄 도구입니다.\n" +
+            "```\n" +
+            " spring init --dependencies=web myproject\n" +
+            "```\n" +
+            "## DB[![](https://raw.githubusercontent.com/aregtech/areg-sdk/master/docs/img/pin.svg)](#db)\n" +
+            "<div align=\"right\">[ <a href=\"#table-of-contents\">↑ to top ↑</a> ]</div>\n" +
+            "\n" +
+            "\n" +
+            "## QueryMethod[![](https://raw.githubusercontent.com/aregtech/areg-sdk/master/docs/img/pin.svg)](#querymethod)\n" +
+            "<div align=\"right\">[ <a href=\"#table-of-contents\">↑ to top ↑</a> ]</div>\n" +
+            "\n" +
+            "- 데이터베이스에서 name이 \"John Doe\"이거나 age가 18 이상인 Person 엔티티를 조회하는 쿼리 메소드\n" +
+            "```\n" +
+            " public interface PersonRepository extends JpaRepository<Person, Long> {\n" +
+            "    List<Person> findByNameOrAgeGreaterThanEqual(String name, int age);\n" +
+            "}\n" +
+            "```\n" +
+            "- 데이터베이스에서 age가 18 이상인 Person 엔티티를 age를 기준으로 오름차순으로 조회하는 쿼리 메소드\n" +
+            "```\n" +
+            " public interface PersonRepository extends JpaRepository<Person, Long> {\n" +
+            "    List<Person> findByAgeGreaterThanEqualOrderByAgeAsc(int age);\n" +
+            "}\n" +
+            "```\n";
+
+        List<String> frameworkNameList = frameworkRepository.findAllName();
+        for(int i = 0 ; i < frameworkNameList.size() ; i++){
+            System.out.println(frameworkNameList.get(i) +" check data");
+            String temp=frameworkNameList.get(i) ;
+            System.out.println(frameworkRepository.findcontent(temp));
+            frame_content=frame_content.replace(temp+"_check",frameworkRepository.findcontent(temp));
+        }
+        frame_content= frame_content.replace("userName",user_name);
+        frame_content= frame_content.replace("repositoryName",repo_name);
+
+        return frame_content;
+    }
 }
