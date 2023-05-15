684dae34c50f362adc29916be7037a782b7d9d5f
YeJi222 <70511859+YeJi222@users.noreply.github.com>
2023-05-15 20:38:49 +0900

    feat: fix error about rm command

--- backend/src/main/java/com/readme/rss/controller/MarkdownController.java
+++ backend/src/main/java/com/readme/rss/controller/MarkdownController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://handongapp.cafe24.com")
+// @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MarkdownController {
     private MdDownloadService mdDownloadService;
